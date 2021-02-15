534cd8f7afcfb6691c98be790d5b9c15dae2b68e
wilsesa <wilson.sanchez14@hotmail.com>
2021-02-15 19:14:36 -0300

    Add project files.

---
+++ Dev.Loja.carros/Models/Carro.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dev.Loja.carros.Models
+{
+    public class Carro
+    {
+        public Guid Id { get; set; }
+        
+        [Required]
+        public string Marca { get; set; }
+
+        [Required]
+        public string Modelo { get; set; }
+
+        [Required]
+        public int Ano { get; set; }
+        public string LinkImg { get; set; }
+
+        public Carro()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
