a1954f26fac67e44603ad876e9961ee9587e3c01
wilsesa <wilson.sanchez14@hotmail.com>
2021-02-15 21:03:16 -0300

    Crud loja carros asp net core

--- Dev.Loja.carros/Data/Migrations/20210215233935_motos.Designer.cs
+++ Dev.Loja.carros/Data/Migrations/20210215233935_motos.Designer.cs
@@ -4,14 +4,16 @@ using Dev.Loja.carros.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Dev.Loja.carros.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210215233935_motos")]
+    partial class motos
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Dev.Loja.carros/Data/Migrations/20210215233935_motos.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Dev.Loja.carros.Data.Migrations
+{
+    public partial class motos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
